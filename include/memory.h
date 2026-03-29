4728141b6b4510768ea22d8761676805af9542b8
jesang-limbu <limbujesang@gmail.com>
2026-03-29 21:47:16 +0100

    build: add memory.h and memory.c

---
+++ include/memory.h
@@ -0,0 +1,19 @@
+#ifndef _MEMORY_H_
+#define _MEMORY_H_
+
+#define MEMORY_SIZE (1 << 20)
+
+typedef struct memory memory_t;
+
+void memory_init(memory_t *mem);
+
+uint8_t memory_read_byte(memory_t *mem, uint32_t addr);
+void memory_write_byte(memory_t *mem, uint32_t addr, uint8_t buf);
+
+uint16_t memory_read_word(memory_t *mem, uint32_t addr);
+void memory_write_word(memory_t *mem, uint32_t addr, uint8_t buf);
+
+uint32_t memory_read_long(memory_t *mem, uint32_t addr);
+void memory_write_long(memory_t *mem, uint32_t addr, uint8_t buf);
+
+#endif
---
+++ src/memory.c
@@ -0,0 +1,5 @@
+#include "memory.h"
+
+struct memory {
+	uint8_t data[MEMORY_SIZE];
+};
